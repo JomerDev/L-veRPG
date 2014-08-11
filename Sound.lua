a903e86e17966b6bf900e54a7063dad3d37c7e11
Nathan1852 <JoniGilmer@web.de>
2014-08-12 00:50:35 +0200

    First draft of the 'Sound' Module The functions have yet to be written

---
+++ Sound.lua
@@ -0,0 +1,38 @@
+--TODO: write the actual functions
+
+Sound = {}
+Sound.Channels = {}
+Sound.VolumeLevels = {}
+Sound.PitchLevels = {}
+
+Sound.play()
+
+Sound.loop()
+
+Sound.pause()
+
+Sound.resume()
+
+Sound.stop()
+
+Sound.update()
+
+Sound.setPitch()
+
+Sound.getPitch()
+
+Sound.setVolume()
+
+Sound.getVolume()
+
+Sound.setTag()
+
+Sound.getTag()
+
+Sound.setEndFunction()
+
+Sound.getEndFunction()
+
+Sound.getPosition()
+
+Sound.setPosition()
